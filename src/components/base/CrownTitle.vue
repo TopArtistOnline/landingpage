2e14ca3c6e46f2690a5f236a34126c54de2f753a
Oscar Manuel <oscar-lez@hotmail.com>
2020-07-14 09:45:28 -0500

    agregar Crown Title

---
+++ src/components/base/CrownTitle.vue
@@ -0,0 +1,37 @@
+<style scoped>
+.container{
+    margin: 1em;
+    display: flex;
+}
+.crown_img{
+    max-width: 5em;
+}
+</style>
+
+<template>
+  <v-container class="container">
+    <v-img
+      :src="require('@/assets'+src)"
+      class="crown_img"
+    />
+    <span
+      class="accent--text text-h5 ma-3"
+    >{{ title }}</span>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'BaseCrownTitle',
+    props: {
+      align: {
+        type: String,
+        default: 'left',
+      },
+      color: String,
+      dark: Boolean,
+      title: String,
+      src: String,
+    },
+  }
+</script>
