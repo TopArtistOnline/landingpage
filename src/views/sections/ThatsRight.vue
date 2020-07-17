89650633321c172e0d04d9aa8347d0be354dd3a1
Oscar Manuel <oscar-lez@hotmail.com>
2020-07-17 11:31:25 -0500

    Changes in view Resources and About

--- src/views/sections/ThatsRight.vue
+++ src/views/sections/ThatsRight.vue
@@ -1,22 +1,45 @@
-<template>
-  <base-section
+c<template>
+  <section
     id="thats-right"
+    class="background"
   >
-    <v-container>
+    <v-container style="display:flex; margin-bottom:-1em;">
       <v-row
-        align="center"
-        justify="center"
+        class="mb-6"
       >
         <v-col
           cols="12"
-          md="7"
+          sm="5"
+          md="6"
         >
-          <h1>Hola Mundo</h1>
+          <v-row>
+            <v-col class="p">
+              <v-img
+                class="svg-icon"
+                :src="require('@/assets/logos/TAO_ISOTIPO.svg')"
+              />
+              <h1 class="white--text display-1 my-3 text-center">
+                <span
+                  style="background-color:#115D74;padding:0 0.1em 0 0.1em;"
+                >That´s Right</span>
+              </h1>
+              <p class="accent--text text-center">
+                Here at Top Artist Online we believe in sharing and giving back,
+              </p>
+              <p class="accent--text text-center">
+                Thats why you´ll earn money and rewards each and every month in exchange for everything you do here with us.
+              </p>
+              <p class="accent--text text-center">
+                Whether you are a content creator uploading quality content, an \n artist setting his/her shop and selling their products(physical of
+              </p>
+            </v-col>
+          </v-row>
         </v-col>
-
         <v-col
-          cols="12"
-          md="5"
+          sm="5"
+          offset-sm="2"
+          md="6"
+          offset-md="0"
         >
           <v-card
             elevation="16"
@@ -24,29 +47,69 @@
           >
             <base-img
               max-width="100%"
-              src="https://cdn.vuetifyjs.com/store/themes/zero/pro.png"
+              height="300px"
+              :src="require('@/assets/elements/caratula_video.png')"
             />
           </v-card>
         </v-col>
       </v-row>
     </v-container>
-  </base-section>
+  </section>
 </template>
-
 <script>
   export default {
     name: 'SectionProChart',
-
     data: () => ({
-      features: [
-        ['Components', 23, '40+'],
-        ['Example Pages', 3, 10],
-        ['Bugfixes and Issues', true, true],
-        ['Vue CLI Support', true, true],
-        ['News, Project, and Pricing Pages', false, true],
-        ['6 Months Free Updates', false, true],
-        ['Price', 'Free', '$60'],
+      items: [
+        { header: 'That´s Right' },
+        { subtitle: 'Here at Top Artist Online we believe in sharing and giving back,' },
+        { paragraph: 'Thats why you´ll earn money and rewards each and every month in exchange for everything you do here with us.' },
+        { paragraph2: 'Whether you are a content creator uploading quality content, an \n artist setting his/her shop and selling their products(physical of' },
+      ],
+      backgroundsitem: [
+        { src: "require('@/assets/elements/relleno_degradado_1.png')" },
+        { src: "require('@/assets/logos/TAO_ISOTIPO.png')" },
       ],
     }),
+    computed: {
+      minHeight () {
+        const height = this.$vuetify.breakpoint.mdAndUp ? '100vh' : '50vh'
+        return `calc(${height} - ${this.$vuetify.application.top}px)`
+      },
+    },
   }
 </script>
+<style scoped>
+  body {
+    font-family: 'Lato', sans-serif;
+}
+h1 {
+    font-family: 'Lato bold', sans-serif;
+}
+ .background{
+   height: 400px;
+   width: 100%;
+   background-image:url('../../assets/backgrounds/background_video.png'),linear-gradient(0deg, rgba(0,130,147,1) 0%, rgba(255,255,255,0) 50%);
+   background-position:right bottom;
+   background-size:auto 54vmax, 100% auto;
+   background-repeat: no-repeat;
+ }
+ .svg-icon{
+   position: absolute;
+   text-align: center;
+   width: 32vmax;
+   filter: invert(26%) sepia(49%) saturate(770%) hue-rotate(147deg) brightness(100%) contrast(92%) opacity(0.15);
+  }
+ .space{
+   justify-content: flex-end;
+ }
+ .margin{
+   margin: -2.5em 1.1em -3em 1.1em;
+ }
+ .blue-line{
+   background-color: #115D74;
+   width: 100%;
+   height: 0.8em;
+   margin: 3em;
+ }
+</style>
--- src/views/sections/ThemeFeatures.vue
+++ src/views/sections/ThemeFeatures.vue
@@ -1,34 +1,167 @@
-<template>
-  <base-section id="theme-features">
-    <div class="text-center text-h4">
-      <v-btn
-        class="ma-2"
-        outlined
-        large
-        fab
-        color="blue"
-      >
-        <v-icon>mdi-pencil</v-icon>
-      </v-btn>
-      <span color="accent">LEARNING</span>
-    </div>
+<style scoped>
+  .background{
+    height: 100%;
+    width: 100%;
+    background-image:url('../../assets/elements/microphone.png'), url('../../assets/backgrounds/background_video_reverse.png'), linear-gradient(0deg, rgba(0,130,147,1) 0%, rgba(255,255,255,0) 50%);
+    background-position: left bottom, left bottom;
+    /*-o-background-size:35% auto, 100% auto;
+    -webkit-background-size:35% auto;
+    -moz-background-size:35% auto;*/
+    background-size: auto 45vmax , auto 45vmax, 100% auto;
+    background-repeat: no-repeat;
+  }
+  .space{
+     justify-content:flex-end;
+  }
+  .blue-line{
+    background-color:#115D74;
+    width: 100%;
+    height: 0.8em;
+    margin-left:3em;
+  }
 
+  .svg-icon{
+    position: absolute;
+    right: -10%;
+    bottom: -5%;
+    width: 32vmax;
+    filter: invert(26%) sepia(49%) saturate(770%) hue-rotate(147deg) brightness(100%) contrast(92%) opacity(0.15);
+  }
+  .svg-icon-2{
+    filter: invert(100%) sepia(6%) saturate(0%) hue-rotate(115deg) brightness(108%) contrast(108%);
+    width:2.8em;
+    top:0.2em;
+  }
+</style>
+
+<template>
+  <section
+    id="theme-features"
+    class="background"
+  >
+    <v-img
+      :src="require('@/assets/logos/TAO_ISOTIPO.svg')"
+      class="svg-icon"
+    />
     <v-container>
       <v-row>
         <v-col
-          v-for="(feature) in features"
-          :key="feature"
-          md="2"
+          v-for="(crowntitle) in crowntitles"
+          :key="crowntitle"
+          style="margin:-2.7em 0 -2.7em 0"
+          md="4"
         >
-          <base-avatar-card-personalizada
-            v-bind="feature"
-            align="left"
+          <base-crown-title
+            v-bind="crowntitle"
             horizontal
           />
         </v-col>
       </v-row>
     </v-container>
-  </base-section>
+
+    <v-img
+      class="blue-line"
+    />
+
+    <v-container>
+      <v-row>
+        <v-col
+          cols="5"
+          sm="5"
+          md="8"
+        >
+          <v-avatar
+            style="border: 0.3em solid #ffc301;
+                  border-radius:50%;
+                  height: 76px;
+                  width: 76px;
+                  background-color:#115D74; float:right;"
+          >
+            <center>
+              <v-img
+                :src="require('@/assets/logos/learning_icon.svg')"
+                class="svg-icon-2"
+              />
+            </center>
+          </v-avatar>
+        </v-col>
+        <v-col>
+          <span
+            class="secondary--text text-h5"
+            style="background-color:#115D74; padding:0.1em 1.5em 0.1em 1.5em; margin: 0.8em 2em 1em -1.5em; position:absolute; text-align:center"
+          >Learning</span>
+        </v-col>
+      </v-row>
+      <v-row class="space">
+        <v-col
+          v-for="(lesson) in lessons"
+          :key="lesson"
+          style="margin:-1em 1.1em -2em 1.1em"
+          md="1"
+        >
+          <base-avatar-card-personalizada
+            v-bind="lesson"
+            horizontal
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          cols="5"
+          sm="5"
+          md="8"
+        />
+        <v-col>
+          <span
+            class="accent--text text-h5"
+            style="margin:-0.4em 2em 1em -1.5em; position:absolute; text-align:center"
+          >and more...</span>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          cols="5"
+          sm="5"
+          md="8"
+        >
+          <v-avatar
+            style="border: 0.3em solid #ffc301;
+                  border-radius:50%;
+                  height: 76px;
+                  width: 76px;
+                  background-color:#115D74; float:right;
+                  left:-1em"
+          >
+            <center>
+              <v-img
+                :src="require('@/assets/logos/learning_icon.svg')"
+                class="svg-icon-2"
+              />
+            </center>
+          </v-avatar>
+        </v-col>
+        <v-col>
+          <span
+            class="secondary--text text-h5"
+            style="background-color:#115D74; padding:0.1em 1.5em 0.1em 1.5em; margin: 0.8em 2em 1em -2.1em; position:absolute; text-align:center"
+          >Marketplace</span>
+        </v-col>
+      </v-row>
+      <v-row class="space">
+        <v-col
+          v-for="(marketplace) in marketplaces"
+          :key="marketplace"
+          style="margin:-1em 1em -2em 0em"
+          md="3"
+        >
+          <base-avatar-card-personalizada
+            v-bind="marketplace"
+            horizontal
+          />
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
 </template>
 
 <script>
@@ -36,32 +169,40 @@
     name: 'SectionThemeFeatures',
 
     data: () => ({
-      features: [
+      lessons: [
         {
-          title: 'Pixel Perfect Design',
-          icon: 'mdi-fountain-pen-tip',
+          title: 'Technique',
+          src: '/circles/circle_01.png',
         },
         {
-          title: 'Retina Ready',
-          icon: 'mdi-cellphone',
+          title: 'Ear training',
+          src: '/circles/circle_02.png',
         },
         {
-          color: 'primary',
-          dark: true,
-          title: 'Easily Customizable',
-          icon: 'mdi-pencil-box-outline',
+          title: 'Music theory',
+          src: '/circles/circle_03.png',
         },
         {
-          title: 'Image Parallax',
-          icon: 'mdi-image-size-select-actual',
+          title: 'Music style patterns',
+          src: '/circles/circle_04.png',
         },
         {
-          title: 'Seo Optimized',
-          icon: 'mdi-ice-pop',
+          title: 'Sound tutorials',
+          src: '/circles/circle_05.png',
+        },
+      ],
+      marketplaces: [
+        {
+          src: '/logos/Logotipo-Ebay.png',
         },
         {
-          title: '24/7 Support',
-          icon: 'mdi-help-circle-outline',
+          src: '/logos/Logotipo-Amazon.png',
+        },
+      ],
+      crowntitles: [
+        {
+          src: '/logos/cup.svg',
+          title: 'Resources',
         },
       ],
     }),
